76ddc98c7fd1b83248831cb1b10d1dafe412dbaa
uvren <berntsson_san@hotmail.com>
2017-05-07 16:46:50 +0200

    Switched connection

--- Development/connection.php
+++ Development/connection.php
@@ -1,4 +1,4 @@
 <?php
-	$conn = mysql_connect("localhost", "root", "bugocheat") or die(mysql_error());
-	$db = mysql_select_db("joel")
+	$conn = mysql_connect("localhost", "joel", "joel") or die(mysql_error());
+	$db = mysql_select_db("joel_data")
 ?>
