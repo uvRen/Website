130f6d3668bf7a71b27ba8d13af49a12ce77eab7
uvren <berntsson_san@hotmail.com>
2017-05-08 12:27:34 +0200

    Save uploaded files as a random named file

--- Development/action.php
+++ Development/action.php
@@ -1,12 +1,16 @@
 <?php
 	// = = = = UPLOAD FILE = = = = 
 	// Must be absolute path
-	$target_directory = "/Library/WebServer/Documents/joel/Website/Development/uploads/";
+	$target_directory = "/var/www/html/Development/uploads/";
 	$filename = $_POST['fileLocation'];
-	$target_file = $target_directory . $filename;
 	$temp_filename = $_FILES["file"]["tmp_name"];
 	$size = $_FILES["file"]["size"];
-	$local_path = "uploads/" . $filename;
+	
+	// Produce a uniq filename
+	$randomNumber = time();
+	$filetype = substr($filename, strrpos($filename, '.'));
+	$target_file = $target_directory . $randomNumber . $filetype;
+	$local_path = "uploads/" . $randomNumber . $filetype;
 
 	// Make sure the webserver process owner owns the destination folder
 	// run exec('whoami'), Mac could be _www
